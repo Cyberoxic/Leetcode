282a44b1766dc234b7869c2923aa9ee3ba6eb25b
Souvik Mondal <souvikmondal607@gmail.com>
2026-06-30 23:34:48 +0530

    Time: 0 ms (100%), Space: 8.1 MB (80.54%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     bool detectCapitalUse(string word) {
         int c = 0;
-        int len = word.length();  
-        for (int i = 0; i < len; i++) 
+        int len = word.length(); 
+        if(len!=0)
         {
-            if (isupper(word[i])) 
-            {    
-                c++;
+            for (int i = 0; i < len; i++) 
+            {
+                if (isupper(word[i])) 
+                {    
+                    c++;
+                }
             }
+            if (c == len || c == 0 || (c == 1 && isupper(word[0]))) 
+            {
+                return true;
+            }
+            return false;
         }
-        if (c == len || c == 0 || (c == 1 && isupper(word[0]))) 
-        {
-            return true;
-        }
-        return false;
+        else return false;
     }
 };

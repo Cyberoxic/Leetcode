920fb63225a393600fc571d0a290373dbd38dbbc
Souvik Mondal <souvikmondal607@gmail.com>
2026-07-02 23:47:16 +0530

    Time: 0 ms (100%), Space: 97.4 MB (24.83%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        int size = prices.size();
+        if (size == 0) return 0;
+        
+        int min_price = prices[0];
+        int max_profit = 0;
+        
+        for (int i = 1; i < size; i++) {
+            if (prices[i] < min_price) {
+                min_price = prices[i];
+            } else {
+                int current_profit = prices[i] - min_price;
+                if (current_profit > max_profit) {
+                    max_profit = current_profit;
+                }
+            }
+        }
+        
+        return max_profit;
+    }
+};

66820800da02b4db1dedc9d13afbf5eecc3368ab
Souvik Mondal <souvikmondal607@gmail.com>
2026-07-05 23:39:41 +0530

    Time: 0 ms (100%), Space: 12.9 MB (93.35%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
-        int ans = 0;
-        for (string& s : sentences) {
-            int word = 1;
-            for (char ch : s) {
-                if (ch == ' ')
+        int ans=0;
+        for (string& s:sentences) {
+            int word=1;
+            for (char ch:s) {
+                if (ch==' ')
                     word++;
             }
-            ans = max(ans, word);
+            ans=max(ans,word);
         }
         return ans;
     }

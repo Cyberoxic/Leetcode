52790a30705a10a81eec45723290b9576b8ca7e7
Souvik Mondal <souvikmondal607@gmail.com>
2026-07-06 23:10:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (43.26%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long num = n;
-        if (num < 0) {
-            x = 1 / x;
-            num = -num;
+        long long num=n;
+        if (num<0) {
+            x=1/x;
+            num=-num;
         }
-        double ans = 1.0;
-        double product = x;
-        while (num > 0) {
-            if (num % 2 == 1) {
-                ans = ans * product;
+        double ans=1.0;
+        double prod=x;
+        while (num>0) {
+            if (num%2==1) {
+                ans=ans*prod;
             }
-            product = product * product;
-            num = num / 2;
+            prod=prod*prod;
+            num=num/2;
         }
         return ans;
     }

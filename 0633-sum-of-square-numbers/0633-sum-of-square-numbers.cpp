055f68430ed72ce82e7047b6b10e91d171a159d4
Souvik Mondal <souvikmondal607@gmail.com>
2026-07-16 15:27:53 +0530

    Time: 0 ms (100%), Space: 7.8 MB (21.81%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c)
+    {
+        int n=sqrt(c);
+        long a=0,b=n;
+        if(c==1 || c==0)
+            return true;
+        while(c>1 && a<=n && b<=n)
+        {
+            long sum= (a*a)+(b*b);
+            if(sum==c)
+                return true;
+            else if(sum>c)
+                b--;
+            else if(sum<c)
+                a++;
+        }
+        return false;
+    }
+};

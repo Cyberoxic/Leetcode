abf3050daec2ded11c33a00d5b8a0bba7e2ad343
Souvik Mondal <souvikmondal607@gmail.com>
2026-07-26 15:36:29 +0530

    Time: 2 ms (78.02%), Space: 7.9 MB (38.78%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,24 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
-        else
+        int a=0,b=1;
+        if(n>1)
         {
-            return fib(n-1)+fib(n-2);
+            for(int i=2; i<=n;i++)
+            {
+                int sum=a+b;
+                a=b;
+                b=sum;
+            }
+            return b;
         }
+        else
+            return n;
+        //recursion technique but it took 9ms to submit
+        //if(n<=1) return n;
+        //else
+        //{
+        //    return fib(n-1)+fib(n-2);
+        //}
     }
 };

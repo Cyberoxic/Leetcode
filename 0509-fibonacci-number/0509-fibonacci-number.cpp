1310c98e05f7ecf22a9e7feb503a0ab5600d6201
Souvik Mondal <souvikmondal607@gmail.com>
2026-07-26 15:23:54 +0530

    Time: 13 ms (31.82%), Space: 7.7 MB (65.52%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n>1)
+        if(n<=1) return n;
+        else
         {
             return fib(n-1)+fib(n-2);
         }
-        else if(n==0) return 0;
-        else return 1;
     }
 };

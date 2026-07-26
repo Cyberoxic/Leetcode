9d77b748c2c9dcd2d2c9f1fed5118f14c1b059f5
Souvik Mondal <souvikmondal607@gmail.com>
2026-07-26 18:35:12 +0530

    Time: 0 ms (100%), Space: 8.1 MB (12.6%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,19 @@
+#include<cmath>
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        /*Recursion but time takes is O(log n)
         if(n<=0) return false;
         if(n==1) return true;
         if(n%2!=0) return false;
         
-        return isPowerOfTwo(n/2);
+        return isPowerOfTwo(n/2);*/
+        if(n<=0) return false;
+        int exponent=log2(n);
+        if(pow(2,exponent)==n)
+        {
+            return true;
+        }
+        return false;
     }
 };

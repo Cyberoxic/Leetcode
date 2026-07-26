9fe05056080a39855a42364dfd5dee6247a6d322
Souvik Mondal <souvikmondal607@gmail.com>
2026-07-26 15:38:42 +0530

    Time: 2 ms (78.02%), Space: 7.8 MB (38.78%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -5,7 +5,7 @@ public:
         int a=0,b=1;
         if(n>1)
         {
-            for(int i=2; i<=n;i++)
+            for(int i=2;i<=n;i++)
             {
                 int sum=a+b;
                 a=b;

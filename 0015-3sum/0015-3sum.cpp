5759f480e77a732536e7ae659bae5451fc2dbe51
Souvik Mondal <souvikmondal607@gmail.com>
2026-07-27 23:01:30 +0530

    Time: 46 ms (68.26%), Space: 31.1 MB (21.43%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,12 +4,14 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
         vector<vector<int>> ans;
-        for (int i=0;i<n;i++) {
+        for (int i=0;i<n;i++) 
+        {
             if (i>0 && nums[i]==nums[i-1])
                 continue;
             int j=i+1;
             int k=n-1;
-            while (j<k) {
+            while (j<k) 
+            {
                 int sum=nums[i]+nums[j]+nums[k];
                 if(sum<0){
                     j++;
@@ -17,7 +19,8 @@ public:
                 else if(sum>0){
                     k--;
                 }
-                else{
+                else
+                {
                     vector<int> temp={nums[i],nums[j],nums[k]};
                     ans.push_back(temp);
                     j++;

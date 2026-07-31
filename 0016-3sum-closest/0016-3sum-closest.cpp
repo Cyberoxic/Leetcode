25c3c1d24b148afd9c862527a31f53134db8c987
Souvik Mondal <souvikmondal607@gmail.com>
2026-08-01 01:43:00 +0530

    Time: 7 ms (86.87%), Space: 14.1 MB (48.95%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -3,34 +3,28 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        int closest_sum=nums[0]+nums[1]+nums[2];
-        for(int i=0;i<n-2;i++) {
-            if(i>0&&nums[i]==nums[i-1]) {
+        int close_sum=nums[0]+nums[1]+nums[2];
+        for (int i=0;i<n-2;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])
                 continue;
-            }
-            int j=i+1;
-            int k=n-1;
-            while(j<k) 
+            int l=i+1;
+            int r=n-1;
+            while(l<r)
             {
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum==target) 
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum==target)
                 {
                     return sum;
                 }
-                if(abs(sum-target)<abs(closest_sum-target)) 
-                {
-                    closest_sum=sum;
-                }
-                if(sum<target) 
-                {
-                    j++;
-                }
+                if(abs(sum-target)<abs(close_sum-target))
+                    close_sum=sum;
+                if(sum<target)
+                    l++;
                 else
-                {
-                    k--;
-                }
-            }
+                    r--;
+            }    
         }
-        return closest_sum;
+        return close_sum;
     }
 };
